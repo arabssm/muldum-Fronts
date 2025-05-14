e153e5dff34006c61262838fa3106e0a69fb9907
123isi <123isi@naver.com>
2025-05-14 16:48:40 +0900

    사이드바

---
+++ src/component/sibebar/siberbarimport.tsx
@@ -0,0 +1,34 @@
+import profile from '@assets/sidebar/profile.svg'; 
+import home from '@assets/sidebar/home.svg';
+import ranking from '@assets/sidebar/ranking.svg';
+import calender from '@assets/sidebar/calender.svg';
+import check from '@assets/sidebar/check.svg';
+import object from '@assets/sidebar/object.svg';
+import alerticon from '@assets/sidebar/alerticon.svg';
+import setting from '@assets/sidebar/setting.svg';
+import Chome from '@assets/sidebar/color-home.svg';
+import Cranking from '@assets/sidebar/color-ranking.svg';
+import Ccalender from '@assets/sidebar/color-calender.svg';
+import Ccheck from '@assets/sidebar/color-check.svg';
+import Cobject from '@assets/sidebar/color-object.svg';
+import Calerticon from '@assets/sidebar/color-alerticon.svg';
+import Gologin from '@assets/sidebar/gologin.svg';
+
+export {
+    profile,
+    home,
+    ranking,
+    calender,
+    check,
+    object,
+    alerticon,
+    setting,
+    Chome,
+    Cranking,
+    Ccalender,
+    Ccheck,
+    Cobject,
+    Calerticon,
+    Gologin
+  };
+  
