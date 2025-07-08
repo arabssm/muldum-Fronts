c62db87989390c85278d93aeac360f38fdfa9663
dpqlsn <a01037111209@gmail.com>
2025-07-08 11:53:39 +0900

    fix (#14) :: 모듈화

--- src/pages/object/object.tsx
+++ src/pages/object/object.tsx
@@ -3,6 +3,7 @@ import * as _ from './style';
 import Sidebar from '@_components/sibebar/sidebar';
 import Box from '@_components/object/box';
 import type { Request } from '@_components/object/types';
+import { initialRequests } from './data';
 
 export default function Object() {
   const remaining = 200000;
@@ -13,37 +14,7 @@ export default function Object() {
   const [qty, setQty] = useState(1);
   const [reason, setReason] = useState('');
 
-
-  const [requests, setRequests] = useState<Request[]>([
-    {
-      no: '01',
-      title: 'AMD 라이젠7 5세대 7800X3D 고급형 멀티팩 정품 국내정식 유통제품',
-      qty: 1,
-      status: '승인대기중',
-      reason: '총학생회 승인 후 바로 구매 예정입니다.',
-    },
-    {
-      no: '02',
-      title: 'USB-C 케이블 1m, 2개입',
-      qty: 2,
-      status: '승인완료',
-      reason: '실험실 장비 연결용으로 필요합니다.',
-    },
-    {
-        no: '02',
-        title: 'USB-C 케이블 1m, 2개입',
-        qty: 2,
-        status: '승인완료',
-        reason: '실험실 장비 연결용으로 필요합니다.',
-      },
-      {
-        no: '02',
-        title: 'USB-C 케이블 1m, 2개입',
-        qty: 2,
-        status: '승인완료',
-        reason: '실험실 장비 연결용으로 필요합니다.',
-      },
-  ]);
+  const [requests, setRequests] = useState<Request[]>(initialRequests);
 
   const handleReasonChange = (no: string, newReason: string) => {
     setRequests(rs =>
