18c4e8f5c0f4a25ebd85237f8c466c05e0e44432
dpqlsn <a01037111209@gmail.com>
2025-07-08 12:08:34 +0900

    fix (#18) :: 메뉴 클릭 시 페이지 이동

--- src/component/onboarding/menu/menu.tsx
+++ src/component/onboarding/menu/menu.tsx
@@ -1,33 +1,28 @@
 import ContentCard from "./contentbox";
 import { items } from "./items";
 import * as _ from './style';
+import { useNavigate } from "react-router-dom";
 
 export default function Menu() {
-  const handleClick = (title: string) => {
-    if(title==='역대 전공동아리 보러가기'){
-        console.log("happy")
-    }else if(title==='물품 신청 하러가기'){
-        console.log("happy")
-    }else if(title==='월말평가 작성하기'){
-        console.log("happy")
-    }else if(title==='공지사항 보러가기'){
-        console.log("happy")
-    }
+  const navigate = useNavigate();
+
+  const handleClick = (path: string) => {
+    navigate(path);
   };
 
   return (
     <_.Wrapper>
-        <_.H1>메뉴</_.H1>
-        <_.H2>원하는 항목을 선택하여 이동하세요</_.H2>
-    <_.GridContainer>
-      {items.map((title) => (
-        <ContentCard
-          key={title}
-          title={title}
-          onClick={() => handleClick(title)}
-        />
-      ))}
-    </_.GridContainer>
+      <_.H1>메뉴</_.H1>
+      <_.H2>원하는 항목을 선택하여 이동하세요</_.H2>
+      <_.GridContainer>
+        {items.map(({ title, path }) => (
+          <ContentCard
+            key={title}
+            title={title}
+            onClick={() => handleClick(path)}
+          />
+        ))}
+      </_.GridContainer>
     </_.Wrapper>
   );
 }
