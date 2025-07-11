883b8f2815651088ebeeb74a2a584d90feae8c04
dpqlsn <a01037111209@gmail.com>
2025-07-11 10:05:02 +0900

    feat :: 역대 전공동아리

--- src/component/sibebar/IconMenu.tsx
+++ src/component/sibebar/IconMenu.tsx
@@ -3,7 +3,7 @@ import { icons } from './icons';
 export const IconMenu = [
     { label: '로그인', path: '', icon: icons.Gologin, iconActive: icons.profile },
     { label: '홈 화면', path: '/', icon: icons.home, iconActive: icons.Chome },
-    { label: '역대 동아리', path: '', icon: icons.ranking, iconActive: icons.Cranking },
+    { label: '역대 동아리', path: '/club-history', icon: icons.ranking, iconActive: icons.Cranking },
     { label: '공유 캘린더', path: '', icon: icons.calender, iconActive: icons.Ccalender },
     { label: '물품 관리', path: '/object', icon: icons.object, iconActive: icons.Cobject },
     { label: '월말평가', path: '', icon: icons.check, iconActive: icons.Ccheck },
